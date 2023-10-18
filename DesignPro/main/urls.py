a7e0cc0d688e7c191188cf93d96bc0dcf9fa8124
gr423_mease <sanymini@gmail.com>
2023-10-18 11:23:58 +0700

    готовая регистрация

---
+++ DesignPro/main/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import index
+from .views import BBLoginView
+from .views import profile
+from .views import BBLogoutView
+from .views import RegisterDoneView, RegisterUserView
+
+app_name = 'main'
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('accounts/login', BBLoginView.as_view(), name='login'),
+    path('accounts/profile/', profile, name='profile'),
+    path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
+    path('accounts/register/done/', RegisterDoneView.as_view(), name='register_done'),
+    path('accounts/register/', RegisterUserView.as_view(), name='register'),
+]
--- DesignPro/main/views.py
+++ DesignPro/main/views.py
@@ -1,6 +1,39 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LogoutView
+from django.views.generic import CreateView
+from .forms import RegisterUserForm
+from django.views.generic import TemplateView
+from .models import AdvUser
+from django.urls import reverse_lazy
 
 
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html')
+
+
+class BBLoginView(LoginView):
+    template_name = 'main/login.html'
+
+
+@login_required
+def profile(request):
+    return render(request, 'main/profile.html')
+
+
+class BBLogoutView(LoginRequiredMixin, LogoutView):
+    template_name = 'main/logout.html'
+
+
+class RegisterUserView(CreateView):
+    model = AdvUser
+    template_name = 'main/register_user.html'
+    form_class = RegisterUserForm
+    success_url = reverse_lazy('main:register_done')
+
+
+class RegisterDoneView(TemplateView):
+   template_name = 'main/register_done.html'
